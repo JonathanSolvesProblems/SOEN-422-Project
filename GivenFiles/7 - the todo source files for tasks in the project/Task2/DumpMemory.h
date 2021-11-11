c3a52976e0ef7c7bb5260c2e6702986ee5a5b7b4
JonathanSolvesProblems <jon_andrei04@hotmail.com>
2021-11-10 20:50:27 -0500

    added Task files that teacher just uploaded on moodle.

---
+++ GivenFiles/7 - the todo source files for tasks in the project/Task2/DumpMemory.h
@@ -0,0 +1,14 @@
+/* DumpMemory.h - Dump Memory Function Interface
+//
+// Copyright (C) 1985-2021 by Michel de Champlain
+//
+*/
+
+#ifndef __DumpMemory_h
+#define __DumpMemory_h
+
+#include <stdint.h>
+
+void DumpMemory(uint8_t* from, uint16_t nBytes);
+
+#endif
---
+++ GivenFiles/7 - the todo source files for tasks in the project/Task2/TestDumpMemory.c
@@ -0,0 +1,32 @@
+/* TestDumpMemory.c - Test Dump Memory Function for Host and Arduino Nano.
+//
+// Copyright (C) 1985-2021 by Michel de Champlain
+//
+*/
+
+#include "DumpMemory.h"
+
+#if !defined(Host)
+#include "bsl_Uart.h"
+#endif
+
+/*-------------------------------------------------------------------
+ * main
+ *-------------------------------------------------------------------*/
+int main(void) {
+    static uint8_t mem[] = {
+         0,  1,  2,  3,  4,  5,  6,  7,  8,  9,
+        10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
+        20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
+        30, 31
+    };
+
+#if !defined(Host)
+    bsl_Uart_Init();
+    DumpMemory( (uint8_t*)0, 32); // 32 registers on Nano.
+#endif
+
+    DumpMemory(mem, 32); // Based 'mem' address (0x0100) on SRAM data.
+
+    return 0;
+}
