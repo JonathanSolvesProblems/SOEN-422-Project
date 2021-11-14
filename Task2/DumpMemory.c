0d81209675cdcd6bc54c7bfe8d340ff4a47508e8
JeffreyLYT <jeffrey.lyt@gmail.com>
2021-11-14 18:08:53 -0500

    add basic task2 setup

--- Task2/DumpMemory.c
+++ Task2/DumpMemory.c
@@ -1,11 +1,20 @@
 #include "DumpMemory.h"
-#include "../Task1/COutForAUnit.h"
 #include <stdint.h>
 
+ void PutC(char c)        { __putchar(c); }
+    void PutS(const char* s) { while (*s) if (s != NULL) PutC(*s++); }
 
 void DumpMemory(uint8_t* from, uint16_t nBytes){
-    uint16_t n;
-    for(n = (uint16_t) *from; n < nBytes; n+=1){ // exclusive for nBytes
-        PutX16(n);
+    uint8_t n;
+    for(n = from; n < nBytes; n++){ // exclusive for nBytes
+        PutS();
+
     }
+
+    //  while (!(UCSR0A & (1 << UDRE0))); // wait for transmit buffer
+    // UDR0 = c; //put data in buffer and transmit
+
+   
+
+
 };
