7d25c1f5423afc6aeaa4fd21547a41dc4fb67222
Jason Kim <jason.k99@hotmail.com>
2021-11-15 12:17:14 -0500

    Got the memory output but prints twice which needs fix

--- Task2/DumpMemory.c
+++ Task2/DumpMemory.c
@@ -5,16 +5,18 @@
 void DumpMemory(uint8_t* from, uint16_t nBytes){
     #if !defined(Host)
 
+    PutS("Shell Nano v1.0\nUsage: type 'm'(memory) and 'q' to quit.");
+
     uint8_t n;
     for(n = 0; n < nBytes; n++){ // exclusive for nBytes
         if(n%16 == 0){
             PutN();
             PutX16((uint16_t) n);
-            PutC('\t');
+            PutC(' ');
         }
+        PutX8((uint8_t) from[n]);
+        PutC(' ');
     }
-    PutX8((uint8_t) from[n]);
-    PutC('\t');
 
     #endif
 
