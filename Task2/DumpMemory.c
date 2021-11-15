aa3e67ba6f5e720b3e19051ef5f5418b7024244a
Jason Kim <jason.k99@hotmail.com>
2021-11-14 20:47:09 -0500

    DumpMemory print is created

--- Task2/DumpMemory.c
+++ Task2/DumpMemory.c
@@ -1,20 +1,21 @@
 #include "DumpMemory.h"
 #include <stdint.h>
 
- void PutC(char c)        { __putchar(c); }
-    void PutS(const char* s) { while (*s) if (s != NULL) PutC(*s++); }
+void PutC(char c)        { __putchar(c); }
+void PutS(const char* s) { while (*s) if (s != NULL) PutC(*s++); }
 
 void DumpMemory(uint8_t* from, uint16_t nBytes){
-    uint8_t n;
-    for(n = from; n < nBytes; n++){ // exclusive for nBytes
-        PutS();
+    #if !defined(Host)
 
+    uint8_t n;
+    for(n = 0; n < nBytes; n++){ // exclusive for nBytes
+        if(n%16 == 0){
+            PutN();
+            PutX16((uint16_t) n);
+            PutC('\t');
+        }
     }
 
-    //  while (!(UCSR0A & (1 << UDRE0))); // wait for transmit buffer
-    // UDR0 = c; //put data in buffer and transmit
-
-   
-
+    #endif
 
 };
---
+++ Task2/Task2AVR.bat
@@ -0,0 +1,4 @@
+avr-gcc -Os -Wall -DF_CPU=16000000UL -mmcu=atmega328p DumpMemory.c TestDumpMemory.c ../Task1/bsl_UsartByPolling.c ../Task1/COutForAUnit_task1_todo.c -o task2.o
+avr-objcopy -O ihex -j .text -j .data task2.o task2.hex
+avrdude -c arduino -p atmega328p -b 57600 -P COM5 -D -Uflash:w:task2.hex:i
+pause
